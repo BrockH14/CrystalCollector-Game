fa7095da8a77a0353017e29ee58bcf484d134d12
BrockH <brock.horton@gmail.com>
2020-02-24 15:31:19 -0500

    base HTML and file structure

---
+++ assets/javascript/Game.js
@@ -0,0 +1,28 @@
+var randNumber = Math.floor(Math.random() * 120) + 19;
+
+var guessedNum =0;
+
+var wins = 0;
+var loses = 0;
+
+
+
+//start game
+
+//display randNumber 
+
+//button 1 add random value 
+
+//button 2 add random value 
+
+//button 3 add random value 
+
+//button 4 add random value 
+
+//update display 
+
+//win condition, match guessNum to randNumber
+
+//lose condition, guessNum over randNumber
+
+//reset game
