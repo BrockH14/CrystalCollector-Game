3bd0de12ea0e0a842a1d1f713d2319c36a41bba2
BrockH <brock.horton@gmail.com>
2020-02-25 09:01:49 -0500

    added function to button, and updating display

--- assets/javascript/Game.js
+++ assets/javascript/Game.js
@@ -1,23 +1,50 @@
-var randNumber = Math.floor(Math.random() * 120) + 19;
-
-var guessedNum =0;
-
+//Global variables
 var wins = 0;
 var loses = 0;
 
-
-
 //start game
+function initializegame(){
+    //local variables
+    var randNumber = Math.floor(Math.random() * 102) + 19;
+    var guessedNum = 0;
+    var addNumber = 0;
+    var rubyNum = Math.floor(Math.random() * 11) + 1;
+    var sappNum = Math.floor(Math.random() * 11) + 1;
+    var topaNum = Math.floor(Math.random() * 11) + 1;
+    var ametNum = Math.floor(Math.random() * 11) + 1;
+
+
+console.log(rubyNum);
+console.log(sappNum);
+console.log(topaNum);
+console.log(ametNum);
+
 
 //display randNumber 
+    $("#RandomNumber").text(randNumber);
+    console.log(randNumber);
 
 //button 1 add random value 
-
+$(".ruby").on("click", function(){
+    guessedNum += rubyNum;
+    $("#UserGuess").text(guessedNum);
+});
 //button 2 add random value 
-
+$(".sapp").on("click", function(){
+    guessedNum += sappNum;
+    $("#UserGuess").text(guessedNum);
+});
 //button 3 add random value 
-
+$(".topa").on("click", function(){
+    guessedNum += topaNum;
+    $("#UserGuess").text(guessedNum);
+});
 //button 4 add random value 
+$(".amet").on("click", function(){
+    guessedNum += ametNum;
+    $("#UserGuess").text(guessedNum);
+});
+
 
 //update display 
 
@@ -25,4 +52,12 @@ var loses = 0;
 
 //lose condition, guessNum over randNumber
 
+
+
+}
+initializegame();
+
 //reset game
+function resetGame(){
+
+}
