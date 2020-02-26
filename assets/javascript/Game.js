d7fa47d896ebc03c2e1bd6c390032dc1d3c921b9
BrockH <brock.horton@gmail.com>
2020-02-26 11:23:00 -0500

    working game code, ready for css

--- assets/javascript/Game.js
+++ assets/javascript/Game.js
@@ -1,75 +1,87 @@
+$(document).ready(function(){
+
 //Global variables
 var wins = 0;
 var loses = 0;
+var base = true;
+var guessedNum = 0;
+var randNumber = 100000;
+var rubyNum = 0;
+var sappNum = 0;
+var topaNum = 0;
+var ametNum = 0;
+
 
 //start game
-function initializegame(){
-    //local variables
-    var randNumber = Math.floor(Math.random() * 102) + 19;
-    var guessedNum = 0;
-    var addNumber = 0;
-    var rubyNum = Math.floor(Math.random() * 11) + 1;
-    var sappNum = Math.floor(Math.random() * 11) + 1;
-    var topaNum = Math.floor(Math.random() * 11) + 1;
-    var ametNum = Math.floor(Math.random() * 11) + 1;
-
+function start(){
+    guessedNum = 0;
+    randNumber = Math.floor(Math.random() * 102) + 19;
+    rubyNum = Math.floor(Math.random() * 11) + 1;   
+    sappNum = Math.floor(Math.random() * 11) + 1;
+    topaNum = Math.floor(Math.random() * 11) + 1;
+    ametNum = Math.floor(Math.random() * 11) + 1;
+    $("#UserGuess").empty();
 
 console.log(rubyNum);
 console.log(sappNum);
 console.log(topaNum);
 console.log(ametNum);
 
-
 //display randNumber 
-    $("#RandomNumber").text(randNumber);
-    console.log(randNumber);
+$("#RandomNumber").text(randNumber);
+
+}
+$(".bt").on("click", function(){
+    $("#UserGuess").text(guessedNum);
+    test();
+    updateDisplay();
+})
 
 //button 1 add random value 
-$(".ruby").on("click", function(){
+$(".ruby").click(function(){
     guessedNum += rubyNum;
-    $("#UserGuess").text(guessedNum);
 });
 //button 2 add random value 
 $(".sapp").on("click", function(){
     guessedNum += sappNum;
-    $("#UserGuess").text(guessedNum);
 });
 //button 3 add random value 
 $(".topa").on("click", function(){
     guessedNum += topaNum;
-    $("#UserGuess").text(guessedNum);
 });
 //button 4 add random value 
 $(".amet").on("click", function(){
     guessedNum += ametNum;
-    $("#UserGuess").text(guessedNum);
 });
-
-//update display 
-
-//win condition, match guessNum to randNumber
-    if (guessedNum === randNumber){
-        wins++
-        resetGame()
-        updateDisplay()
-    }
-
-//lose condition, guessNum over randNumber
-    if (guessedNum > randNumber){
-        loses++
-        resetGame()
-        updateDisplay()
-    }
-
+function test(){
+if (guessedNum === randNumber){
+    wins++;
+    resetgame();
 }
-
+else if (guessedNum > randNumber){
+    loses++;
+    resetgame();
+}
+else if (guessedNum < randNumber){
+    updateDisplay();
+}
+}
+//update display 
 function updateDisplay(){
     $("#Wins").text(wins);
     $("#Losses").text(loses);
+    $("#UserGuess").text(guessedNum);
 }
 //reset game
-function resetGame(){
-    initializegame();
+
+function resetgame(){
+ 
+    guessedNum = 0;
+    rubyNum = 0;
+    sappNum = 0;
+    topaNum = 0;
+    ametNum = 0;
+    start();
 }
-initializegame();
-updateDisplay();
+start();
+});
